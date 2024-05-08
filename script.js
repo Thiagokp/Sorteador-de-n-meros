10b21ca38b8b009282de198d489c5acb7c968809
Thiagokp <thiagovb128@gmail.com>
2024-05-08 18:53:32 -0300

    Inclusão da regra (Valor inicial >= Valor final)

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ buttonClick.addEventListener("click", function () {
     const valorInicial = Math.ceil(document.querySelector("#valor-inicial").value)
     const valorFinal = Math.floor(document.querySelector("#valor-final").value)
 
-    const ResultadoFinal = Math.floor(Math.random() * (valorFinal - valorInicial + 1)) + valorInicial;
-
+    if (valorInicial >= valorFinal) {
+        alert("O valor minimo tem que ser MENOR que o valor máximo !")
+    } else {
+        const ResultadoFinal = Math.floor(Math.random() * (valorFinal - valorInicial + 1)) + valorInicial;
+    }       
     alert(ResultadoFinal)
 })
 
